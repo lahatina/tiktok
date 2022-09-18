53f3f3ce5c2e24453eff5fbffc2d4e82a1ce16da
Quyen Tran <nhoquyen28@gmail.com>
2022-09-18 15:55:57 +0700

    Cấu hình router/layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-
-// import { useRef, useState } from 'react';
-// import './App.css';
-
-
-
-
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
 
 function App() {
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
 
-  return (
-    <div>
+                        let Layout = DefaultLayout;
 
-    </div>
-  );
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+// Layout
+import { HeaderOnly } from '~/components/Layout';
+// Pages import
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+// publicRoutes
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+// Need Login to Routes
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
